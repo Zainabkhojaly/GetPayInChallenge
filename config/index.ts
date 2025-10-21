7e1960a9811029ba8b213ae8bfe1d49b809c00bb
Zainab Khojaly <zainab.m.khojaly@gmail.com>
2025-10-21 19:31:52 +0300

    Update index.ts

---
+++ config/index.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://dummyjson.com';
+export const SUPERADMIN_USERNAME = 'kminchelle'; // Superadmin username
+export const LOCK_TIMEOUT = 10000; // 10 seconds for auto-lock
